642814af92eabb843f7565b583459a59b54f8fa7
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2022-11-12 09:53:36 +0000

    Updated script.js

--- js/script.js
+++ js/script.js
@@ -5,8 +5,8 @@ function getNumberInfo() {
     // Get the value of the input field
     let num = document.getElementById("mynumber").value;
     // Check if input is valid
-    if (isNaN(num) || num.length == 0 || num < 10 || num > 10000 || (num.length > 1 && num[0] == "0") || !Number.isInteger(Number(num))) {
-        txt += `Invalid Input.  Please enter a whole number between 10 and 10000.  Do not include leading zeros.`;
+    if (isNaN(num) || num.length == 0 || num < 10 || num > 1000 || (num.length > 1 && num[0] == "0") || !Number.isInteger(Number(num))) {
+        txt += `Invalid Input.  Please enter a whole number between 10 and 1000.  Do not include leading zeros.`;
     } else {
         txt += `You have entered the number ${num}. <p>`;
         txt += `In the first ${num} expansions, ${squareRootConvergents(num)} fraction(s) contain a numerator with more digits than the denominator.`
